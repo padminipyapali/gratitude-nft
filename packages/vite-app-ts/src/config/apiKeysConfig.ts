2a26e34a5e4ce085bbe79e32c6e0031ee96cf932
Padmini Pyapali <padmini.pyapali@gmail.com>
2022-01-18 13:19:03 -0500

    Initial commit

---
+++ packages/vite-app-ts/src/config/apiKeysConfig.ts
@@ -0,0 +1,13 @@
+/** ******************************
+ * API KEYS
+ ****************************** */
+// API key overview:  https://docs.ethers.io/v5/api-keys/
+// MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
+
+export const INFURA_ID = "7d7e7fedbfb04c27b55ef19358814bf2";
+
+// MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
+export const ETHERSCAN_KEY = "KV73BUW93PISVT6VIE5EVDJ7W18AJSWBJ9";
+
+// BLOCKNATIVE ID FOR Notify.js:
+export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
